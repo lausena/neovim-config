9e66728d4f08b977486e072d30792bfce2d06606
lausena <lau.sena.gabriel@gmail.com>
2026-02-09 19:49:23 -0500

    lazy loading a few plugins

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -1,13 +1,15 @@
 return {
   "vim-test/vim-test",
+  lazy = false,
   dependencies = {
-    "preservim/vimux"
+    "preservim/vimux",
+    lazy = false
   },
   vim.keymap.set('n', '<leader>t', ':TestNearest<CR>'),
   vim.keymap.set('n', '<leader>T', ':TestFile<CR>'),
   vim.keymap.set('n', '<leader>a', ':TestSuite<CR>'),
   vim.keymap.set('n', '<leader>l', ':TestLast<CR>'),
   vim.keymap.set('n', '<leader>g', ':TestVisit<CR>'),
-
+  
   vim.cmd("let  test#strategy = 'vimux'"),
 }
